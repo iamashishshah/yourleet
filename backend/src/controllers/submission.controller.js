7e92d38ed0588e3fcdc341b4680a6a50b93f35bd
Ashish Shah <eiepixxstuff@gmail.com>
2025-05-16 02:16:44 +0530

    feat(controller): add getSubmissions to fetch user submissions for a specific problem

--- backend/src/controllers/submission.controller.js
+++ backend/src/controllers/submission.controller.js
@@ -39,5 +39,52 @@ export const getAllSubmissions = async (req, res) => {
         });
     }
 };
-export const getSubmission = async (req, res) => {};
+
+export const getSubmissions = async (req, res) => {
+    // get the all submissions that a user has submitted for a problem
+    const userId = req.user.id;
+    const problemId = req.params?.problemId;
+
+    if (!problemId) {
+        return res.status(400).json({
+            success: false,
+            message: "Missing or invalid problem ID.",
+        });
+    }
+
+    const { page = 1, limit = 10 } = req.query;
+    const skip = (page - 1) * limit;
+
+    try {
+        const submissions = await db.submission.findMany({
+            where: { userId, problemId },
+            orderBy: { createdAt: "desc" },
+            take: Number(limit),
+            skip: Number(skip),
+            select: {
+                id: true,
+                language: true,
+                status: true,
+                time: true,
+                memory: true,
+                createdAt: true,
+                sourceCode: true,
+            },
+        });
+
+        res.status(200).json({
+            success: true,
+            message: "All submissions for the given problem.",
+            page: Number(page),
+            limit: Number(limit),
+            submissions,
+        });
+    } catch (error) {
+        console.error(`[ERROR] Failed to fetch submissions for problem ${problemId}:`, error);
+        return res.status(500).json({
+            success: false,
+            message: "Unable to retrieve submissions at the moment. Please try again later.",
+        });
+    }
+};
 export const getSubmissionCountForProblem = async (req, res) => {};
--- backend/src/routes/submission.route.js
+++ backend/src/routes/submission.route.js
@@ -1,10 +1,10 @@
 import express from "express";
 import { authMiddleware } from "../middlewares/auth.middleware.js";
-import { getAllSubmissions, getSubmission, getSubmissionCountForProblem } from "../controllers/submission.controller.js";
+import { getAllSubmissions, getSubmissionCountForProblem, getSubmissions } from "../controllers/submission.controller.js";
 
 const submissionRoutes = express.Router();
 submissionRoutes.get("/", authMiddleware, getAllSubmissions);
-submissionRoutes.get("/problem/:problemId", authMiddleware ,getSubmission)
+submissionRoutes.get("/problem/:problemId", authMiddleware ,getSubmissions)
 submissionRoutes.get("/problem/:problemId/count", authMiddleware, getSubmissionCountForProblem)
 
 export default submissionRoutes;
