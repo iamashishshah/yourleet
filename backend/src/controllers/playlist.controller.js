3401fc99e458642b1afea18d900391cb0e9c50ad
iamashishshah <eiepixxstuff@gmail.com>
2025-05-20 21:29:26 +0530

    feat(controller): add get playlist controller using playlist id

--- backend/src/controllers/playlist.controller.js
+++ backend/src/controllers/playlist.controller.js
@@ -1,6 +1,7 @@
 import { db } from "../libs/db.js";
 
 export async function getAllPlaylists(req, res) {
+    //TODO: test this controller
     const userId = req.user?.id;
 
     if (!userId) {
@@ -22,7 +23,7 @@ export async function getAllPlaylists(req, res) {
 
         if (playlists.length === 0) {
             return res.status(404).json({
-                success: true,
+                success: false,
                 message: "No playlists found for this user.",
                 playlists: [],
             });
@@ -42,7 +43,51 @@ export async function getAllPlaylists(req, res) {
     }
 }
 
-export async function getPlaylist(req, res) {}
+export async function getPlaylist(req, res) {
+    const { playlistId } = req.params;
+
+    if (!playlistId) {
+        return res.status(400).json({
+            success: false,
+            message: "Playlist ID is missing or invalid.",
+        });
+    }
+
+    try {
+        const playlist = await db.playlist.findUnique({
+            where: {
+                playlistId,
+            },
+            include: {
+                problems: {
+                    include: {
+                        problem: true,
+                    },
+                },
+            },
+        });
+
+        if (!playlist) {
+            return res.status(404).json({
+                success: false,
+                message: "Playlist doesn't exist.",
+            });
+        }
+
+        res.status(200).json({
+            success: true,
+            message: "Playlist fetched successfully.",
+            playlist,
+        });
+    } catch (error) {
+        console.error(`[ERROR] Failed to fetch playlist for user ${req.user.id}:`, error);
+        res.status(500).json({
+            success: false,
+            message: "Internal server error. Please try again later.",
+        });
+    }
+}
+
 export async function createPlaylist(req, res) {}
 export async function addProblemInPlaylist(req, res) {}
 export async function deletePlaylist(req, res) {}
